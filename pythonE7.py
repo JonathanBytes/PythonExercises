c12b935fae6fd83a26b7ecceab1ff255838f3821
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-02-11 19:51:15 -0500

    Update

---
+++ pythonE7.py
@@ -0,0 +1,3 @@
+filename = input("Ingrese el nombre del archivo: ")
+filenameSep = filename.split(".")
+print('La extensión del archivo es:',filenameSep[1])
---
+++ pythonE8.py
@@ -0,0 +1,2 @@
+color_list = ["Red","Green","White" ,"Black"]
+print('El primer color de la lista es:',color_list[0],'y el último es:',color_list[-1])
