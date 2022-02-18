f314843f596fb0fc0578e19c17eb0e08eacadfdb
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-02-18 14:16:53 -0500

    Update

---
+++ pythonE10.py
@@ -0,0 +1,3 @@
+n = int(input('Ingrese un entero: '))
+m = str(n); a = int(2*m); b = int(3*m)
+print(n+a+b)
---
+++ pythonE9.py
@@ -0,0 +1,14 @@
+exam_st_date = (11, 12, 2014)
+
+day = exam_st_date[0]
+month = exam_st_date[1]
+year = exam_st_date[2]
+
+print('El día de examinación será el:',end=' ')
+print(day,month,year,sep=' / ')
+
+# Another method
+
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
