f6e20ed577d8823a03531619d829612bab3c6e6f
Jonathan ArchDell <jonathancardonaramirez@gmail.com>
2022-02-28 14:39:05 -0500

    Update

--- pythonE10.py
+++ pythonE10.py
@@ -2,3 +2,6 @@ n = int(input('Ingrese un entero: '))
 m = str(n); a = int(2*m); b = int(3*m)
 print('%s + %s%s + %s%s%s = ' %(m,m,m,m,m,m), end='')
 print(n+a+b)
+
+# Alternativa
+print(n+n*11+n*111)
